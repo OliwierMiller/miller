248aeda0427894d90a7b426a3163302189f021df
Marcin Wysocki <wysockib00@gmail.com>
2019-12-03 09:30:27 +0100

    tablice

---
+++ z2.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#cytowanie "" double quote
+echo -e "Polecenie \t ls\n"
+
+#użycie ``
+echo "wynik ls: `ls`"
+
+#apostrof single quote ''
+echo 'Polecenie \t ls \n'
+
+#zmienne 
+x=10
+
+echo "Zmienna x: ${x}"
+echo 'Zmienna x: ${x}'
+
+#pwd
+WD=`pwd`
+
+echo ${WD}
+
+#zmienne tablicowe
+tab=(1 3 5)
+echo ${tab[0]}
+echo ${tab[1]}
+echo ${tab[2]}
+
+echo ${tab[*]}
+
+
+
+
